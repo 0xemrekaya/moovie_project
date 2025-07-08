762fdc84883f1d3fa3ae39413e1e457202f4caf8
Emre Kaya <emrekaya75@outlook.com>
2025-07-09 00:48:47 +0300

    Implement healthcheck endpoint and error handling; add movie data structure and routing

---
+++ cmd/api/helpers.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+type envelope map[string]any
+
+// Retrieve the "id" URL parameter from the current request context, then convert it to
+// an integer and return it.
+func (app *application) readIDParam(r *http.Request) (int64, error) {
+	params := httprouter.ParamsFromContext(r.Context())
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	if err != nil || id < 1 {
+		return 0, errors.New("invalid id parameter")
+	}
+	return id, nil
+}
+
+// Define a writeJSON() helper for sending responses.
+func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
+	// Encode the data to JSON, returning the error if there was one.
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	// Append a newline to make it easier to view in terminal applications.
+	js = append(js, '\n')
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+	// Add the "Content-Type: application/json" header, then write the status code and JSON response.
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+	return nil
+}
---
+++ cmd/api/movie.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"moovie/internal/data"
+	"net/http"
+	"time"
+)
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "create a new movie")
+}
+
+// Add a showMovieHandler for the "GET /v1/movies/:id" endpoint.
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	// Create a new instance of the Movie struct, containing the ID we extracted from
+	// the URL and some dummy data.
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+	// Encode the struct to JSON and send it as the HTTP response.
+	err = app.writeJSON(w, http.StatusOK, movie, nil)
+	if err != nil {
+		app.logger.Error(err.Error())
+		app.serverErrorResponse(w, r, err)
+	}
+
+}
